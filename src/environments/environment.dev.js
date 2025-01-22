8fe142a05f92a5e63a8915d7abace5011049c2b0
minhajmhd <minhajmuhammedv@gmail.com>
2025-01-22 18:01:46 +0530

    .env file

--- src/environments/environment.dev.js
+++ src/environments/environment.dev.js
@@ -1,4 +1,4 @@
-// const hosted_url = "https://ems-backend-c517.onrender.com/api";
-const localHostUrl = 'http://localhost:5000/api'
+const hosted_url = "https://ems-backend-c517.onrender.com/api";
+// const localHostUrl = 'http://localhost:5000/api'
 
-export const baseUrl = localHostUrl; 
+export const baseUrl = hosted_url; 
